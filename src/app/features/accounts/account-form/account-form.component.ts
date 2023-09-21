31b28500c8508bc33985f711ea6e0b4e18974d0c
Wesley Valentim <wv.reis@gmail.com>
2023-09-21 11:46:47 -0300

    Begin of account form implementation .

--- src/app/features/accounts/account-form/account-form.component.ts
+++ src/app/features/accounts/account-form/account-form.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder } from '@angular/forms';
 import { AccountService } from './../account.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
@@ -9,18 +10,21 @@ import { Account } from 'src/app/core/models/account.model';
   templateUrl: './account-form.component.html',
   styleUrls: ['./account-form.component.css']
 })
-export class AccountFormComponent implements OnInit, OnDestroy {  
+export class AccountFormComponent implements OnInit, OnDestroy {
   account!: Account;
   loading: boolean = true;
   destroy$ = new Subject<void>();
 
   constructor(
     private readonly accountService: AccountService,
-    private readonly activatedRoute: ActivatedRoute
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly formBuilder: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.GetAccount();
+
+    
   }
   ngOnDestroy(): void {
     this.destroy$.next();
@@ -30,7 +34,7 @@ export class AccountFormComponent implements OnInit, OnDestroy {
   GetAccount(): void{
     this.loading = true;
     var id = +this.activatedRoute.snapshot.paramMap.get('id')!;
-    
+
     this.accountService
       .GetAccount(id)
       .pipe(takeUntil(this.destroy$))
