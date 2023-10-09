048e28ef918514f85929094533dc168d4be3a2e4
Wesley Valentim <wv.reis@gmail.com>
2023-10-09 01:03:07 -0300

    Beginning of global error handler.

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable, catchError, throwError } from 'rxjs';
+import { ErrorPopupComponent } from 'src/app/shared/error-popup/error-popup.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Injectable({ providedIn: "root" })
 export class ErrorInterceptor implements HttpInterceptor {
-    intercept(
-        req: HttpRequest<any>, 
-        next: HttpHandler
-    ): Observable<HttpEvent<any>> {
-        return next.handle(req).pipe(
-            catchError((error) => {
-                return throwError(() => error);
-            })            
+
+  constructor(
+    private readonly ngbModal: NgbModal
+  ) {
+  }
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      catchError((error) => {
+        this.ngbModal.open(
+          error.details === undefined ? error.name : error.details,
+          { centered: true }
         );
-    }
+
+        return throwError(() => error);
+      })
+    );
+  }
 }
